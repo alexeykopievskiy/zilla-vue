6965c8fb32bdea7374a1b5a8f574169bdbc3cd78
Alexey Kopievskiy <alexeykopievskiy@gmail.com>
2020-04-11 00:53:22 +0300

    add basic markup

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,40 @@
   <v-app>
     <v-content>
       <v-row justify="center">
-        <v-dialog v-model="dialog" persistent max-width="290">
+        <v-dialog persistent max-width="600">
           <template v-slot:activator="{ on }">
             <v-btn justify="center" color="primary" dark v-on="on">Open Dialog</v-btn>
           </template>
           <v-card>
-            <v-card-title class="headline">Use Google's location service?</v-card-title>
-            <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="green darken-1" text @click="dialog = false">Disagree</v-btn>
-              <v-btn color="green darken-1" text @click="dialog = false">Agree</v-btn>
-            </v-card-actions>
+            <v-tabs
+              v-model="tab"
+              background-color="deep-purple accent-4"
+              class="elevation-2"
+              dark
+              :centered="centered"
+              :grow="grow"
+              :vertical="vertical"
+              :right="right"
+              :prev-icon="prevIcon ? 'mdi-arrow-left-bold-box-outline' : undefined"
+              :next-icon="nextIcon ? 'mdi-arrow-right-bold-box-outline' : undefined"
+              :icons-and-text="icons"
+            >
+              <v-tabs-slider></v-tabs-slider>
+
+              <v-tab v-for="(tab, index) in tabs" :key="tab" :href="`#tab-${index}`">
+                {{tab}}
+              </v-tab>
+
+              <v-tab-item :value="'tab-0'">
+                <Deposit />
+              </v-tab-item>
+              <v-tab-item :value="'tab-1'">
+                <Withdraw />
+              </v-tab-item>
+              <v-tab-item :value="'tab-2'">
+                <Rebalance />
+              </v-tab-item>
+            </v-tabs>
           </v-card>
         </v-dialog>
       </v-row>
@@ -22,11 +44,28 @@
 </template>
 
 <script>
+import Deposit from '@/components/Deposit'
+import Withdraw from '@/components/Withdraw'
+import Rebalance from '@/components/Rebalance'
+
 export default {
   name: "App",
+  components: {
+    Deposit, Withdraw, Rebalance
+  },
   data() {
     return {
-      dialog: false
+      tab: null,
+      text:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      icons: false,
+      centered: false,
+      grow: false,
+      vertical: false,
+      prevIcon: false,
+      nextIcon: false,
+      right: false,
+      tabs: ['Deposit', 'Withdraw', 'Rebalance']
     };
   }
 };
---
+++ src/components/Rebalance.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-card>
+    <v-card-text>Rebalance</v-card-text>
+  </v-card>
+</template>
